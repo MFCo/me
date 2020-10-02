3141f8fe24104581ab46d5cb26d4613d164f1bb6
MFCo <marianococirio@gmail.com>
2020-10-02 03:14:28 +0200

    add title

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,5 @@
 import React from "react";
-import Document from "next/document";
+import Document, { Head, Main, NextScript, Html } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 export default class MyDocument extends Document {
@@ -26,5 +26,19 @@ export default class MyDocument extends Document {
     } finally {
       sheet.seal();
     }
+  }
+
+  render() {
+    return (
+      <Html lang="en" dir="ltr">
+        <Head>
+          <title> Mariano Cocirio </title>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
   }
 }
