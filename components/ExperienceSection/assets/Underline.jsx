2e7b7a9d67abff026869b715d9730ee95ad22da0
MFCo <marianococirio@gmail.com>
2020-10-03 16:07:36 +0200

    underline

--- components/ExperienceSection/assets/Underline.jsx
+++ components/ExperienceSection/assets/Underline.jsx
@@ -2,10 +2,11 @@ import styled from "styled-components";
 import { motion } from "framer-motion";
 
 const Underline = styled(motion.div)`
-  max-width: 110%;
-  margin-top: -${({ theme }) => theme.unit * 2}px;
-  height: ${({ theme }) => theme.unit * 2}px;
+  width: 100%;
+  height: ${({ theme }) => theme.unit}px;
+  border-radius: ${({ theme }) => theme.unit / 2}px;
   background: ${({ color }) => color};
+  bottom: -${({ theme }) => theme.unit}px;
   @media only screen and (max-width: ${({ theme }) =>
       theme.breakpoint.mobile}) {
     margin-bottom: ${({ theme }) => theme.unit * 3}px;
