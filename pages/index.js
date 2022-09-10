59c244d4671fd73fe439f7163e6806f00c5a63d9
MFCo <marianococirio@gmail.com>
2022-09-10 22:16:29 +0200

    SEO opt

--- pages/index.js
+++ pages/index.js
@@ -81,9 +81,9 @@ export default function IndexPage({ posts }) {
           posts.slice(0, 5).map(
             ({ _id, title = "", slug = "", publishedAt = "" }) =>
               slug && (
-                <li className="pb-1 mb-1" key={_id}>
+                <li className="pb-1 mb-2" key={_id}>
                   <Link href="/blog/[slug]" as={`/blog/${slug.current}`}>
-                    <a className="italic underline mb-1">{title}</a>
+                    <a className="italic underline">{title}</a>
                   </Link>{" "}
                   <span className="text-sm">
                     ({new Date(publishedAt).toLocaleDateString("en-GB")})
