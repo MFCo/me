61b59fb278fd8340eedaac8968bb25e1b061edfc
MFCo <marianococirio@gmail.com>
2022-10-26 09:32:12 +0200

    analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
 import "../styles/globals.css";
 import "tailwindcss/tailwind.css";
 import { ThemeProvider } from "next-themes";
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function App({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
         <Component {...pageProps} />
+        <Analytics />
     </ThemeProvider>
   );
 }
